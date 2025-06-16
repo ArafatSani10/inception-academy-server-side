f9878aa1be4d82a3d49c986cdde2feb8946263f7
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2025-06-16 22:39:33 +0600

    Academy course ui done and details done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middlewares
 app.use(cors());
@@ -17,43 +17,57 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // academy course related apis
+        const academyCollection = client.db('academyCourseDb').collection('academyCourse');
+
+        app.get('/academyCourse', async (req, res) => {
+            const cursor = academyCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
 
-    
+
+
+        app.get('/academyCourse/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await academyCollection.findOne(query);
+            res.send(result);
+        });
 
 
 
 
 
-
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send('inception academy is running')
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`inception academy is running on port ${port}`)
 })
